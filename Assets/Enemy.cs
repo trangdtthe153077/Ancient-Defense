9c869a3e96ba87387502046492a68c5ee71620b8
Dang Thi Thuy Trang (k15 HL) <nguyenchattrang@gmail.com>
2023-03-12 22:56:09 +0700

    Revert "update from main"
    This reverts commit 75731d75c361b7b141eb52c42416dd9acee1b26c, reversing
    changes made to 20d5f3b14d3b6ed1c7ef24cc2461a7b0b4e625b6.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,25 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
+
 public class Enemy : MonoBehaviour
 {
-    public float speed = 5f;
-    public Transform target;
-    public float attackDistance = 1f;
+    // Start is called before the first frame update
 
-    private Rigidbody2D rb;
-    int health = 10;
-    public int maxHealth = 10;
-    private int currentHealth;
-    public int damage = 10; // Lượng sát thương gây ra khi tấn công
-    public float attackDelay = 2f; // Thời gian giữa hai lần tấn công
-    private float attackTimer; // Thời gian từ lần tấn công cuối cùng
-    public GameObject coinPrefab; // đối tượng tiền tệ
-    public int coinCount = 1; // số lượng tiền tệ rơi ra
-    private bool isDead = false; // kiểm tra quái đã chết hay chưa
+    public int maxHealth = 15;
+    public int currentHealth;
 
-    public GameObject Player;
+
 
     void Start()
     {
@@ -28,32 +18,13 @@ public class Enemy : MonoBehaviour
            transform.position = new Vector3(screenBounds.x, Random.Range(-screenBounds.y, screenBounds.y), 0f);*/
 
 
-
-        rb = GetComponent<Rigidbody2D>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // quái di chuyển sang trái
         transform.Translate(Vector3.left * 3f * Time.deltaTime);
         /* GetComponent<Rigidbody2D>().AddForce(-transform.right * 0.5f);*/
-        // Tính toán thời gian giữa hai lần tấn công
-        attackTimer += Time.deltaTime;
-        if (attackTimer >= attackDelay)
-        {
-            attackTimer = 0f;
-
-            // Tấn công nếu người chơi ở trong vùng va chạm
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
-            foreach (Collider2D collider in colliders)
-            {
-                if (collider.tag == "Player")
-                {
-                    collider.GetComponent<Player>().TakeDamage(damage);
-                }
-            }
-        }
-
     }
 
     /*    private void OnTriggerEnter2D(Collider2D collision)
@@ -71,6 +42,9 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        Debug.Log("Auuu");
+        Debug.Log(damage);
+        Debug.Log(currentHealth);
         currentHealth -= damage;
         Debug.Log(currentHealth);
         if (currentHealth <= 0)
@@ -78,6 +52,7 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+
     private void Die()
     {
         // Add code here to handle enemy death (e.g. play death animation, spawn loot, etc.)
@@ -99,44 +74,3 @@ public class Enemy : MonoBehaviour
             }*/
     }
 }
-    
-    /*    else if (other.gameObject.CompareTag("Arrow"))
-        {
-            Debug.Log("Trung dan");
-            Arrow arrow = other.GetComponent<Arrow>();
-            TakeDamage(arrow.damage);
-        }
-        //if (collision.gameObject.CompareTag("Player"))
-        //{
-        //    Tiền sẽ rơi ra tại vị trí hiện tại của quái vật
-        //    Instantiate(coinPrefab, transform.position, Quaternion.identity);
-        //    Destroy quái vật
-        //    Destroy(gameObject);
-        //}
-    }
-    void MoveTowardsPlayer()
-    {
-        Vector2 direction = target.position - transform.position;
-        rb.velocity = direction.normalized * speed;
-    }
-
-    void Attack()
-    {
-        // Add code to damage the player here
-    }
-    //void Fire()
-    //{
-    //    // Create a new arrow and set its position and rotation
-    //    GameObject arrow = Instantiate(arrowPrefab, transform.position, transform.rotation);
-
-    //    // Apply a force to the arrow to make it move forward
-    //    arrow.GetComponent<Rigidbody2D>().AddForce(transform.right * 1500f);
-    //    arrow.GetComponent<Arrow>().damage = Mathf.RoundToInt(archer.damage);
-    //}
-}
-
-
-
-
-
-
