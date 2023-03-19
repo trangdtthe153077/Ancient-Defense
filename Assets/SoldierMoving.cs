6a36644e17362ac375d5bc11788f07f739d72092
ducnmhe150286 <97494895+ducnmhe150286@users.noreply.github.com>
2023-03-19 23:17:22 +0700

    update soldier attack

--- Assets/SoldierMoving.cs
+++ Assets/SoldierMoving.cs
@@ -1,56 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class SoldierMoving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed;
+	private Rigidbody2D rb;
+
+	public int damage = 1;
+	// Start is called before the first frame update
+	public float speed;
     void Start()
     {
         
     }
-	//void StopMoving()
-	//{
-	//	Dừng di chuyển của quái
-	//	rb.velocity = Vector2.zero;
-	//	rb.angularVelocity = 0f;
-	//	rb.Sleep();
-	//}
-	//IEnumerator RotateObject()
-	//{
-	//	while (true)
-	//	{
-	//		transform.Rotate(0, 0, 1);
-	//		yield return null;
-	//	}
-	//}
+	void StopMoving()
+	{
+		// Dừng di chuyển của quái
+		rb.velocity = Vector2.zero;
+		rb.angularVelocity = 0f;
+		rb.Sleep();
+	}	
+	IEnumerator RotateObject()
+	{
+		int numRotations = 0;
+		float totalRotation = 0f;
+		while (numRotations < 1)
+		{
+			transform.Rotate(0, 0, 10);
+			totalRotation += 10f;
+			yield return null;
+			if (totalRotation >= 360f)
+			{
+				numRotations++;
+				Debug.Log("Finish turn " + numRotations);
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update()
     {
-		//foreach (Collider2D collider in colliders)
-		//{
-		//	if (collider.tag == "enemy")
-		//	{
-		//		Enemy enemy = collider.GetComponent<Enemy>();
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2f);
+		foreach (Collider2D collider in colliders)
+		{
+			if (collider.tag == "Enemy")
+			{
+				Enemy enemy = collider.GetComponent<Enemy>();
 
 
-		//		if (enemy != null)
-		//		{
+				if (enemy != null)
+				{
+					enemy.TakeDamage(damage);
+					StartCoroutine(RotateObject());
+					StopMoving();
 
-		//			StartCoroutine(RotateObject());
-		//			tower.TakeDamage(damage);
-		//			StopMoving();
-
-		//		}
-		//		else
-		//		{
-		//			Tower component không tồn tại trên đối tượng
-		//			Debug.Log("Tower component not found!");
-		//		}
-		//	}
-		//}
+				}
+				else
+				{
+					//Tower component không tồn tại trên đối tượng
+					Debug.Log("Tower component not found!");
+				}
+			}
+		}
 		transform.position += Vector3.right * speed * Time.deltaTime;
     }
 	
