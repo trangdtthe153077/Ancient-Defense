d9b0822d7c31d99cebd95fe86d466ee8d72ab7a4
Dang Thi Thuy Trang (k15 HL) <nguyenchattrang@gmail.com>
2023-03-23 19:20:38 +0700

    lop linh

---
+++ Assets/BasicArcherController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicArcherController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform pos1;
+    public Transform pos2;
+    public Transform pos3;
+    public Transform pos4;
+    public Transform pos5;
+
+    public GameObject archer;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
